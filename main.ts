ea5441110d90e4bb3e90eec81c8da43a424dd0d8
Ammaar-e <62914394+Ammaar-e@users.noreply.github.com>
2023-10-17 13:32:42 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Talha")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(200)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Ammaar")
+})
+basic.showIcon(IconNames.Yes)
+basic.pause(100)
+basic.clearScreen()
